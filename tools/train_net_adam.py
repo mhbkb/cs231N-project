2d1462288e300e417787bcf9a9c1acdae4380faf
hao mao <hao.mao@haodeAir.home>
2021-05-25 14:01:11 -0700

    fix

--- tools/train_net_adam.py
+++ tools/train_net_adam.py
@@ -122,11 +122,8 @@ class Trainer(DefaultTrainer):
         """
         params = get_default_optimizer_params(
             model,
-            base_lr=cfg.SOLVER.BASE_LR,
             weight_decay=cfg.SOLVER.WEIGHT_DECAY,
             weight_decay_norm=cfg.SOLVER.WEIGHT_DECAY_NORM,
-            bias_lr_factor=cfg.SOLVER.BIAS_LR_FACTOR,
-            weight_decay_bias=cfg.SOLVER.WEIGHT_DECAY_BIAS,
         )
 
         return maybe_add_gradient_clipping(cfg, torch.optim.Adam)(params, cfg.SOLVER.BASE_LR)
